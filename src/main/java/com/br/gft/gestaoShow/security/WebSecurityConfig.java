c72f4dcfe331f321da21bb85aa713597ee29083a
Vinicius Farias Santos <spviniciusfariassantos@gmail.com>
2020-02-27 14:53:30 -0300

    Projeto Spring com logout no cabecalho

--- src/main/java/com/br/gft/gestaoShow/security/WebSecurityConfig.java
+++ src/main/java/com/br/gft/gestaoShow/security/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http.csrf().disable().authorizeRequests()
-		.antMatchers(HttpMethod.GET, "/home").permitAll()
+		.antMatchers(HttpMethod.GET, "/").permitAll()
 		.anyRequest().authenticated()
 		.and().formLogin().permitAll()
 		.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
